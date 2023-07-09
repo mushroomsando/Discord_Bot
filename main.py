140b8c814c3b968fcd7bde47f0b4dee5f69dc7cd
JUULG-SCHOOLPC\windows <mushroomsando@naver.com>
2023-07-09 13:57:43 +0900

    Using the colorama module

--- main.py
+++ main.py
@@ -2,40 +2,43 @@ import discord
 from colorama import init, Fore, Style
 from discord.ext import commands
 import os
-import tracemalloc
 import time
 
 init() # Color initialization
-now = time.strftime("\033[90m"+"%Y-%m-%d %H:%M:%S"+"\033[0m", time.localtime()) # Set Current time
+now = time.strftime(f"{Fore.LIGHTBLACK_EX}%Y-%m-%d %H:%M:%S{Style.RESET_ALL}", time.localtime()) # Set Current time
 
-#TODO Replace color output code with colora module
 # Create Discord Client
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
-tracemalloc.start()
 
 # token file load
 def load_token():
     with open('token.txt', 'r') as file:
         return file.read().strip()
 
-#Cogs load
+# Cogs load
 async def load_cogs():
     for filename in os.listdir('cogs'):
         if filename.endswith('.py'):
             try:
                 await bot.load_extension(f'cogs.{filename[:-3]}')
-                print(now + "\033[32m" + " PASS     "+ "\033[0m"+"\033[35m"+"Cogs load process"+"\033[0m"+f" cogs.{filename[:-3]} Load successful.")
+                print(f"{now}{Fore.GREEN} PASS     {Fore.MAGENTA}Cogs load process{Style.RESET_ALL} cogs.{filename[:-3]} Load successful.")
             except Exception as e:
-                print(now + "\033[31m" + " ERROR    "+"\033[0m"+"\033[35m"+"Cogs load process"+"\033[0m"+f" cogs.{filename[:-3]} Load Faile")
+                print(f"{now}{Fore.RED} ERROR    {Fore.MAGENTA}Cogs load process{Style.RESET_ALL} cogs.{filename[:-3]} Load Failed.")
                 print(f'Error: {type(e).__name__} - {e}')
 
-#Login
+# Login
 @bot.event
 async def on_ready():
     await load_cogs()
     print("=====================")
-    print(now + "\033[36m" + " INFO     "+ "\033[0m"+"\033[35m"+"Login process"+"\033[0m"+f' LOGIN  : {bot.user.name}')
-    print(now + "\033[36m" + " INFO     "+ "\033[0m"+"\033[35m"+"Login process"+"\033[0m"+f" BOT ID : {bot.user.id}")
+    color_code = '3b8eea'
+    print(f'\033[38;2;{int(color_code[:2], 16)};{int(color_code[2:4], 16)};{int(color_code[4:], 16)}m' +
+        f"{now}{Fore.CYAN} INFO     \033[38;2;{int(color_code[:2], 16)};{int(color_code[2:4], 16)};{int(color_code[4:], 16)}m" +
+        f"{Fore.MAGENTA}Login process{Style.RESET_ALL} LOGIN  : {bot.user.name}")
+
+    print(f'\033[38;2;{int(color_code[:2], 16)};{int(color_code[2:4], 16)};{int(color_code[4:], 16)}m' +
+        f"{now}{Fore.CYAN} INFO     \033[38;2;{int(color_code[:2], 16)};{int(color_code[2:4], 16)};{int(color_code[4:], 16)}m" +
+        f"{Fore.MAGENTA}Login process{Style.RESET_ALL} BOT ID : {bot.user.id}")
     print("=====================")
 
 # Bot run
